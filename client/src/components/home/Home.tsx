e151fa7fa3f193be3b7e91c09356ba6548b940d0
YotamZiv298 <yotamziv87@gmail.com>
2021-07-31 21:02:45 +0300

    Changed styles

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,36 +1,62 @@
 import React from 'react';
 
 import './Home.css';
-import { Button, Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
 import Chat from '../chat/Chat';
 import ChatList from '../chatList/ChatList';
-import { useLocation, withRouter } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import { useChats } from '../../context/ChatProvider';
 
-const Home = () => {
-    const location = useLocation<any>();
+type HomeProps = {
+    id: string;
+};
+
+const Home = (props: HomeProps) => {
+    const { selectedChat } = useChats();
+
+    if (!props.id || !props.id.length) return <Redirect to='/login' />;
+
+    // const location = useLocation<any>();
 
     return (
         <React.Fragment>
-            <Container fluid className='home-container'>
+            <Container
+                fluid
+                className='home-container'
+                style={{ overflow: 'hidden' }}
+            >
                 <Row>
                     <Col>
                         <div className='logout-container'>
                             <h4 style={{ color: 'white' }}>
-                                Hello, {location.state.nickname}
+                                {/* Hello, {location.state.nickname} */}
+                                {props.id}
                             </h4>
                             <Button
                                 variant='danger'
-                                onClick={() => window.location.reload()}
+                                onClick={() => {
+                                    localStorage.clear();
+                                    window.location.reload();
+                                }}
                             >
                                 Logout
                             </Button>{' '}
                         </div>
                     </Col>
                 </Row>
-                <Row>
-                    <Col style={{ height: '90vh', overflow: 'auto' }}>
-                        <ChatList chatList={[]} />
+                <Row style={{ justifyContent: 'space-between' }}>
+                    <Col
+                        md={2}
+                        style={{
+                            height: '90vh',
+                            maxWidth: '100%' /*, overflow: 'auto' */,
+                        }}
+                    >
+                        <ChatList id={props.id} />
+                    </Col>
+                    <Col md={{ span: 8, offset: 0 }}>
+                        {selectedChat && <Chat />}
                     </Col>
                 </Row>
             </Container>
@@ -38,4 +64,5 @@ const Home = () => {
     );
 };
 
-export default withRouter(Home);
+// export default withRouter(Home);
+export default Home;
