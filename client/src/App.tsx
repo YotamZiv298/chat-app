58ea801877f14bd7f786b771081e7ce2f06f4bb1
YotamZiv298 <yotamziv87@gmail.com>
2021-07-31 21:00:17 +0300

    Added providers and id

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-
 import './App.css';
 
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    Redirect,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import Login from './components/login/Login';
-import PrivateRoute from './components/PrivateRoute';
-import { io } from 'socket.io-client';
+import useLocalStorage from './hooks/useLocalStorage';
+import { SocketProvider } from './context/SocketProvider';
+import { ContactsProvider } from './context/ContactsProvider';
+import { ChatsProvider } from './context/ChatProvider';
 
 const App = () => {
-    const [isAuth, setIsAuth] = useState(false);
-    const [id, setId] = useState();
+    // const [isAuth, setIsAuth] = useState(false);
+    const [id, setId] = useLocalStorage('id', [] as any);
+
+    const home = (
+        <SocketProvider id={id}>
+            <ContactsProvider>
+                <ChatsProvider id={id}>
+                    <Home id={id} />
+                </ChatsProvider>
+            </ContactsProvider>
+        </SocketProvider>
+    );
 
     return (
         <Router>
             <div className='App'>
-                <PrivateRoute exact path='/' component={Home} isAuth={isAuth} />
-                <Route
-                    exact
-                    path='/login'
-                    component={() => (
-                        <Login
-                            isAuth={isAuth}
-                            setIsAuth={setIsAuth}
-                            onIdSubmit={setId}
-                        />
-                    )}
-                />
+                <Switch>
+                    <Route exact path='/' component={() => home} />
+                    <Route
+                        exact
+                        path='/login'
+                        component={() => <Login onIdSubmit={setId} />}
+                    />
+                </Switch>
             </div>
         </Router>
+        // <Router>
+        //     <div className='App'>
+        //         <PrivateRoute exact path='/' component={Home} isAuth={isAuth} />
+        //         <Route
+        //             exact
+        //             path='/login'
+        //             component={() => (
+        //                 <Login
+        //                     isAuth={isAuth}
+        //                     setIsAuth={setIsAuth}
+        //                     onIdSubmit={setId}
+        //                 />
+        //             )}
+        //         />
+        //     </div>
+        // </Router>
     );
 };
 
