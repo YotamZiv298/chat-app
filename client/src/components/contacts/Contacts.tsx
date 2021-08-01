51d69f369937cf7adf45cc521813f2200d352400
YotamZiv298 <yotamziv87@gmail.com>
2021-08-01 19:26:08 +0300

    Wrapped in container

--- client/src/components/contacts/Contacts.tsx
+++ client/src/components/contacts/Contacts.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import { ListGroup } from 'react-bootstrap';
 import { useContacts } from '../../context/ContactsProvider';
 
+import './Contacts.css';
+
 const Contacts = () => {
     const { contacts } = useContacts();
 
     return (
         <React.Fragment>
-            <ListGroup variant='flush'>
-                {contacts.map((contact: any) => (
-                    <ListGroup.Item key={contact.id}>
-                        {contact.name}
-                    </ListGroup.Item>
-                ))}
-            </ListGroup>
+            <div className='contacts-container'>
+                <ListGroup variant='flush'>
+                    {contacts.map((contact: any) => (
+                        <ListGroup.Item key={contact.id}>
+                            {contact.name}
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
+            </div>
         </React.Fragment>
     );
 };
