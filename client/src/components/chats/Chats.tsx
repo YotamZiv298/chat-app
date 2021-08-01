67f2424288304ab8c850fe292962d0fdea6a5f5c
YotamZiv298 <yotamziv87@gmail.com>
2021-08-01 19:26:50 +0300

    Wrapped in container

--- client/src/components/chats/Chats.tsx
+++ client/src/components/chats/Chats.tsx
@@ -5,21 +5,23 @@ const Chats = () => {
     const { chats, selectChatIndex } = useChats();
 
     return (
-        <ListGroup variant='flush' >
-            {chats.map((chat: any, index: number) => (
-                <ListGroup.Item
-                    key={index}
-                    action
-                    onClick={() => selectChatIndex(index)}
-                    active={chat.selected}
-                    // style={{ backgroundColor: '#2a2f32' }}
-                >
-                    {chat.recipients
-                        .map((r: { name: any }) => r.name)
-                        .join(', ')}
-                </ListGroup.Item>
-            ))}
-        </ListGroup>
+        <div className='chats-container'>
+            <ListGroup variant='flush'>
+                {chats.map((chat: any, index: number) => (
+                    <ListGroup.Item
+                        key={index}
+                        action
+                        onClick={() => selectChatIndex(index)}
+                        active={chat.selected}
+                        // style={{ backgroundColor: '#2a2f32' }}
+                    >
+                        {chat.recipients
+                            .map((r: { name: any }) => r.name)
+                            .join(', ')}
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
+        </div>
     );
 };
 export default Chats;
