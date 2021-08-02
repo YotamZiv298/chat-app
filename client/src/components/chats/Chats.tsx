d56a11ccce2267dec0e5237f06cf86230f49d26b
YotamZiv298 <yotamziv87@gmail.com>
2021-08-02 15:38:05 +0300

    Added fragment container

--- client/src/components/chats/Chats.tsx
+++ client/src/components/chats/Chats.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ListGroup } from 'react-bootstrap';
 import { useChats } from '../../context/ChatProvider';
 
@@ -5,23 +6,25 @@ const Chats = () => {
     const { chats, selectChatIndex } = useChats();
 
     return (
-        <div className='chats-container'>
-            <ListGroup variant='flush'>
-                {chats.map((chat: any, index: number) => (
-                    <ListGroup.Item
-                        key={index}
-                        action
-                        onClick={() => selectChatIndex(index)}
-                        active={chat.selected}
-                        // style={{ backgroundColor: '#2a2f32' }}
-                    >
-                        {chat.recipients
-                            .map((r: { name: any }) => r.name)
-                            .join(', ')}
-                    </ListGroup.Item>
-                ))}
-            </ListGroup>
-        </div>
+        <React.Fragment>
+            <div className='chats-container'>
+                <ListGroup variant='flush'>
+                    {chats.map((chat: any, index: number) => (
+                        <ListGroup.Item
+                            key={index}
+                            action
+                            onClick={() => selectChatIndex(index)}
+                            active={chat.selected}
+                            // style={{ backgroundColor: '#2a2f32' }}
+                        >
+                            {chat.recipients
+                                .map((r: { name: any }) => r.name)
+                                .join(', ')}
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
+            </div>
+        </React.Fragment>
     );
 };
 export default Chats;
