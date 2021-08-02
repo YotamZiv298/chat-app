87cdbc74a4e9a866cd4343e8cbe35501453cc029
YotamZiv298 <yotamziv87@gmail.com>
2021-08-02 21:00:47 +0300

    Updated types

--- api/src/users/user.model.ts
+++ api/src/users/user.model.ts
@@ -1,7 +1,9 @@
+import { Contact } from 'src/contacts/contact.model';
+
 export class User {
     constructor(
         public id: string,
-        public chats: any[],
-        public contacts: { id: string; name: string }[]
+        public chats: { recipients: any; messages: any[] }[],
+        public contacts: Contact[]
     ) {}
 }
