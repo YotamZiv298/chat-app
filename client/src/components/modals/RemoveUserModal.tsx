4e919ee73b64a7f9dd9ce92c0351bb0f5b42308c
YotamZiv298 <yotamziv87@gmail.com>
2021-08-03 15:19:07 +0300

    Remove user modal

---
+++ client/src/components/modals/RemoveUserModal.tsx
@@ -0,0 +1,71 @@
+import React, { useRef, useState } from 'react';
+import { Button, Form, Modal } from 'react-bootstrap';
+import { useChats } from '../../context/ChatProvider';
+import { useContacts } from '../../context/ContactsProvider';
+
+type RemoveUserModalProps = {
+    closeModal: () => any;
+};
+
+const RemoveUserModal = (props: RemoveUserModalProps) => {
+    const [selectedContactIds, setSelectedContactIds] = useState<any>([]);
+    const { contacts } = useContacts();
+    const { selectedChat } = useChats();
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (!selectedContactIds.length) {
+            alert('Remove contacts to create a chat');
+            return;
+        }
+        if (
+            selectedChat.recipients.some((r: any) =>
+                selectedContactIds.includes(r.id)
+            )
+        ) {
+            alert('Contact already in chat');
+            return;
+        }
+
+        // createChat(selectedContactIds);
+        props.closeModal();
+    };
+
+    const handleCheckboxChange = (contactId: any) => {
+        setSelectedContactIds((prev: any[]) => {
+            if (prev.includes(contactId)) {
+                return prev.filter((prevId) => {
+                    return contactId !== prevId;
+                });
+            } else {
+                return [...prev, contactId];
+            }
+        });
+    };
+
+    return (
+        <React.Fragment>
+            <Modal.Header closeButton>Remove User</Modal.Header>
+            <Modal.Body>
+                <Form onSubmit={handleSubmit}>
+                    {contacts.map((contact: any) => (
+                        <Form.Group controlId={contact.id} key={contact.id}>
+                            <Form.Check
+                                type='checkbox'
+                                value={selectedContactIds.includes(contact.id)}
+                                label={contact.name}
+                                onChange={() =>
+                                    handleCheckboxChange(contact.id)
+                                }
+                            />
+                        </Form.Group>
+                    ))}
+                    <Button variant='danger' type='submit'>Remove</Button>
+                </Form>
+            </Modal.Body>
+        </React.Fragment>
+    );
+};
+
+export default RemoveUserModal;
